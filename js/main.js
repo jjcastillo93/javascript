275c976b1e025f0b80df30753d6476176d4e9585
jjcastillo93 <juancr2004@gmail.com>
2023-05-02 11:36:18 -0300

    fueron agregadas funciones a la simulacion

--- js/main.js
+++ js/main.js
@@ -1,27 +1,42 @@
-let meses30 = ["abril", "junio", "septiembre", "noviembre"];
-let meses31 = ["enero", "marzo", "mayo", "julio", "agosto", "octubre", "diciembre"];
-let febrero = "febrero";
+const meses30 = ["abril", "junio", "septiembre", "noviembre"];
+const meses31 = ["enero", "marzo", "mayo", "julio", "agosto", "octubre", "diciembre"];
+const febrero = "febrero";
 let seguirIngresando = true;
-while (seguirIngresando) {
+
+function esDiaValido(mes, dia) {
+  if (meses30.includes(mes)) {
+    return dia >= 1 && dia <= 30;
+  } else if (meses31.includes(mes)) {
+    return dia >= 1 && dia <= 31;
+  } else if (mes === febrero) {
+    return dia >= 1 && dia <= 28;
+  } else {
+    return false;
+  }
+}
+
+function realizarReserva() {
   let mesIngresado = prompt("Ingrese el mes que desea reservar (por ejemplo, enero, febrero, marzo, etc.):");
   mesIngresado = mesIngresado.toLowerCase();
   let diaIngresado = parseInt(prompt("Ingrese el dia que desea reservar (por ejemplo, 1, 2, 3, etc.):"));
-  if (meses30.includes(mesIngresado)) {
-  } 
-  else if (meses31.includes(mesIngresado)) {
-  } 
-  else if (mesIngresado === febrero) {
+  
+  if (!esDiaValido(mesIngresado, diaIngresado)) {
+    alert("El día ingresado no es válido para el mes de " + mesIngresado + ". Por favor, verifique los datos.");
+    return;
   }
-  else {
-    alert("El dia o el mes ingresado no es válido. Por favor, verifique los datos");
-    continue;
-  }
+  
   let nombre = prompt("Ingrese su nombre:");
   let apellido = prompt("Ingrese su apellido:");
   alert("La Reserva para " + nombre + " " + apellido + " en el mes de " + mesIngresado + " y el día " + diaIngresado + " fue exitosa");
+}
+
+while (seguirIngresando) {
+  realizarReserva();
+  
   let respuesta = prompt("¿Quieres hacer otra reserva? (si o no)");
   if (respuesta.toLowerCase() !== "sí" && respuesta.toLowerCase() !== "si") {
     seguirIngresando = false;
   }
 }
+
 alert("Programa de reserva finalizado");
