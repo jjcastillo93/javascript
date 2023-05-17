fd87cd7bf8bde3b93e2308b898bb1532bcf16d31
jjcastillo93 <juancr2004@gmail.com>
2023-05-16 23:27:14 -0300

    agregue cambios encontrados en las correciones de la preentrega uno como comentar codigo  y en la validacion ya que se encontraba que por prompt se podia pasar sin ingresar los datos deseados de tal manera que use varios confirm que permitieran una respuesta mas precisa al igual que trabaje en los nombres de las variables creadas

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
+/*estas constantes almacenan los meses segun los que los dias que poseen cada uno es decir si llega a 31, 30, 28*/
 const meses30 = ["abril", "junio", "septiembre", "noviembre"];
 const meses31 = ["enero", "marzo", "mayo", "julio", "agosto", "octubre", "diciembre"];
 const febrero = "febrero";
-let seguirIngresando = true;
+let continuarReservando = true;
+/* reservas se completara con un push utilizando la constante reserva*/
 let reservas = [];
-
+/* esta funcion permite la diferencion en los dias de las constantes segun su mes si es mes30, mes31, mes febrero:28*/
 function esDiaValido(mes, dia) {
   if (meses30.includes(mes)) {
     return dia >= 1 && dia <= 30;
@@ -15,7 +17,7 @@ function esDiaValido(mes, dia) {
     return false;
   }
 }
-
+/* esta funcion permite ingresar meses y dias siempre en cuando se cumpla las condiciones del esDiaValido de lo contrario mandara un alert y pedira ingresar nuevamente*/
 function realizarReserva() {
   let mesIngresado = prompt("Ingrese el mes que desea reservar (por ejemplo, enero, febrero, marzo, etc.):");
   mesIngresado = mesIngresado.toLowerCase();
@@ -25,7 +27,7 @@ function realizarReserva() {
     alert("El día ingresado no es válido para el mes de " + mesIngresado + ". Por favor, verifique los datos.");
     return;
   }
-
+/* una vez validado esDiaValido pedira ingresar nombre, apellido y una descrion*/
   let nombre = prompt("Ingrese su nombre:");
   let apellido = prompt("Ingrese su apellido:");
   let descripcion = prompt("Ingrese motivo de consulta:");
@@ -41,18 +43,18 @@ function realizarReserva() {
   reservas.push(reserva);
   alert("La reserva para " + nombre + " " + apellido + " en el mes de " + mesIngresado + " y el día " + diaIngresado + " fue exitosa " + "para atender su consulta " + descripcion);
 }
-
-while (seguirIngresando) {
+/*ciclo while para seguir ingresando reserva*/
+while (continuarReservando) {
   realizarReserva();
 
   let respuesta = confirm("¿Quieres hacer otra reserva?");
   if (!respuesta) {
-    seguirIngresando = false;
+    continuarReservando = false;
   }
 }
 
 console.log(reservas);
-
+/*este cliclo busca eliminar una vez que eliminarReserva sea verdadero las reservas encontradas en el array*/
 let eliminarReserva = confirm("¿Desea eliminar una reserva?");
 while (eliminarReserva) {
   let nombreEliminar = prompt("Ingrese el nombre asociado a la reserva que desea eliminar:");
